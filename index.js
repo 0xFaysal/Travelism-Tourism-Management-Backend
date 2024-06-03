9ed03e0dca3353c6c22cb7d49a4eedc239d9116e
Faysal Ahmed Fahim <ffahim2330717@bscse.uiu.ac.bd>
2024-06-03 13:04:48 +0600

    Refactor API endpoints and improve error handling

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 
 const port = 3000;
 
+// Import the required MongoDB classes
 const {MongoClient, ServerApiVersion} = require("mongodb");
 const uri = process.env.MONGODB_URI;
 
@@ -20,34 +21,36 @@ const client = new MongoClient(uri, {
     },
 });
 
+// Define the POST method route
 async function run() {
     try {
         await client.connect();
-
         const database = client.db("Travelism");
         const collectionUser = database.collection("users");
         const collectionPost = database.collection("posts");
 
         app.post("/api/v1/insert/user", async (req, res) => {
-            console.log(req.body.userID);
+            console.log(req.body.userId);
             const doc = req.body;
+
             const allUsers = await collectionUser.find({}).toArray();
             const isUserExist = allUsers.some(
-                (user) => user.userID === doc.userID
+                (user) => user.userId === doc.userId
             );
+
             const numberOfElements = Object.keys(doc).length;
 
             if (!doc) {
                 res.header("status", "400");
-                console.log("Invalid input 1");
+                console.log("Invalid input: doc is empty");
                 res.send({status: 400, message: "Invalid input"});
-            } else if (!doc.userID) {
+            } else if (!doc.userId) {
                 res.header("status", "400");
-                console.log("Invalid input 2");
+                console.log("Invalid input: user id not found");
                 res.send({status: 400, message: "Invalid input"});
             } else if (!doc.userEmail) {
                 res.header("status", "400");
-                console.log("Invalid input 3");
+                console.log("Invalid input: user email not found");
                 res.send({status: 400, message: "Invalid input"});
             } else if (isUserExist) {
                 res.header("status", "400");
@@ -63,62 +66,86 @@ async function run() {
                     );
                 } else {
                     res.header("status", "400");
-                    console.log("Invalid input 4");
+                    console.log(
+                        "Invalid input: number of elements not equal as expected"
+                    );
                     res.send({status: 400, message: "Invalid input"});
                 }
             }
         });
 
         app.post("/api/v1/insert/post", async (req, res) => {
-            console.log(req.body.userID);
+            console.log(req.body.userId);
             const doc = req.body;
             const allUsers = await collectionUser.find({}).toArray();
 
             const isUserExistID = allUsers.some(
-                (user) => user.userID === doc.userID
+                (user) => user.userId === doc.userId
             );
             const isUserExistEmail = allUsers.some(
                 (user) => user.userEmail === doc.userEmail
             );
+
             const isUserExist = isUserExistID && isUserExistEmail;
+
+            console.log("Is the user exist:" + isUserExist);
+
+            const numberOfElements = Object.keys(doc).length;
 
             if (!doc) {
                 res.header("status", "400");
-                console.log("Invalid input 1");
+                console.log("Invalid input: doc is empty");
                 res.send({status: 400, message: "Invalid input"});
-            } else if (!doc.userID) {
+            } else if (!doc.userId) {
                 res.header("status", "400");
-                console.log("Invalid input 2");
+                console.log("Invalid input: user id not found");
                 res.send({status: 400, message: "Invalid input"});
             } else if (!doc.userEmail) {
                 res.header("status", "400");
-                console.log("Invalid input 3");
+                console.log("Invalid input: user email not found");
                 res.send({status: 400, message: "Invalid input"});
             } else if (!isUserExist) {
                 res.header("status", "400");
-                console.log("Invalid input 4");
-                res.send({status: 400, message: "User does not exist"});
+                console.log("Invalid input: User does not exist");
+                res.status(400).send({message: "User does not exist"});
             } else {
-                const result = await collectionPost.insertOne(doc);
-                res.header("status", "200");
-                res.send(result);
-                console.log(
-                    `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
-                );
+                if (numberOfElements === 13) {
+                    const result = await collectionPost.insertOne(doc);
+                    res.header("status", "200");
+                    res.send(result);
+                    console.log(
+                        `A document was inserted with the _id: ${result.insertedId}`
+                    );
+                } else {
+                    res.header("status", "400");
+                    console.log(
+                        "Invalid input: number of elements not equal as expected"
+                    );
+                    res.send({status: 400, message: "Invalid input"});
+                }
             }
         });
 
-        app.get("/api/v1/get/data=:userID", async (req, res) => {
-            const userID = req.params.userID;
-            console.log(userID);
-            const allUserData = await collectionUser.find().toArray();
-            res.header("status", "200");
-            res.send(allUserData);
+        app.get("/api/v1/get/data=:parameter", async (req, res) => {
+            const parameter = req.params.parameter;
+            console.log(parameter);
+            const allUserData = await collectionPost.find().toArray();
+            if (parameter === "all") {
+                res.header("status", "200");
+                res.send(allUserData);
+            } else {
+                const filteredUserData = allUserData.filter(
+                    (user) => user.userID === parameter
+                );
+                res.header("status", "200");
+                res.send(filteredUserData);
+            }
         });
     } finally {
         // await client.close();
     }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
