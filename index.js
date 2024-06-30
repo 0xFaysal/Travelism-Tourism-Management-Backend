9b36dff0d94f6ba057e9005d330753223bc7df63
Faysal Ahmed Fahim <ffahim2330717@bscse.uiu.ac.bd>
2024-06-30 18:38:56 +0600

    chore: Refactor API endpoints and improve error handling

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const port = 3000;
 
 // Import the required MongoDB classes
-const {MongoClient, ServerApiVersion} = require("mongodb");
+const {MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
 const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -129,18 +129,115 @@ async function run() {
         app.get("/api/v1/get/data=:parameter", async (req, res) => {
             const parameter = req.params.parameter;
             console.log(parameter);
-            const allUserData = await collectionPost.find().toArray();
+            const countryName = [
+                "bangladesh",
+                "thailand",
+                "indonesia",
+                "malaysia",
+                "vietnam",
+                "cambodia",
+            ];
+            const isCountryExist = countryName.some(
+                (country) => country.toLowerCase() === parameter.toLowerCase()
+            );
+            const postCollectionData = await collectionPost.find().toArray();
+            const allUsers = await collectionUser.find().toArray();
+            const isUserExistID = allUsers.some(
+                (user) => user.userId === parameter
+            );
 
             if (parameter === "all") {
                 res.header("status", "200");
-                res.send(allUserData);
-            } else {
-                const filteredUserData = allUserData.filter(
+                res.send(postCollectionData);
+            } else if (isUserExistID) {
+                const filteredUserData = postCollectionData.filter(
                     (user) => user.userId === parameter
                 );
-                console.log(filteredUserData);
+                // console.log(filteredUserData);
                 res.header("status", "200");
                 res.send(filteredUserData);
+            } else if (ObjectId.isValid(parameter)) {
+                const result = await collectionPost.findOne({
+                    _id: new ObjectId(parameter),
+                });
+                console.log("hi", result);
+                res.header("status", "200");
+                res.send(result);
+            } else if (isCountryExist) {
+                const result = await collectionPost.find().toArray();
+                console.log(result);
+                const filteredCountryData = result.filter(
+                    (post) =>
+                        post.country_name.toLowerCase() ===
+                        parameter.toLowerCase()
+                );
+                console.log(filteredCountryData, isCountryExist);
+                res.header("status", "200");
+                res.send(filteredCountryData);
+            }
+        });
+
+        app.put("/api/v1/update/data=:parameter", async (req, res) => {
+            const parameter = req.params.parameter;
+            const doc = req.body;
+            const allUsers = await collectionUser.find().toArray();
+            const isUserExistID = allUsers.some(
+                (user) => user.userId === doc.userId
+            );
+            const isUserExistEmail = allUsers.some(
+                (user) => user.userEmail === doc.userEmail
+            );
+            const isUserExist = isUserExistID && isUserExistEmail;
+
+            if (!doc) {
+                res.header("status", "400");
+                console.log("Invalid input: doc is empty");
+                res.send({status: 400, message: "Invalid input"});
+            } else if (!doc.userId) {
+                res.header("status", "400");
+                console.log("Invalid input: user id not found");
+                res.send({status: 400, message: "Invalid input"});
+            } else if (!doc.userEmail) {
+                res.header("status", "400");
+                console.log("Invalid input: user email not found");
+                res.send({status: 400, message: "Invalid input"});
+            } else if (!isUserExist) {
+                res.header("status", "400");
+                console.log("Invalid input: User does not exist");
+                res.status(400).send({message: "User does not exist"});
+            } else {
+                const result = await collectionPost.updateOne(
+                    {_id: new ObjectId(parameter)},
+                    {$set: doc}
+                );
+                console.log(
+                    `${result.matchedCount} document(s) matched the query criteria.`
+                );
+                res.header("status", "200");
+                res.send({status: 200, message: "Data updated successfully"});
+            }
+        });
+
+        app.delete("/api/v1/delete/data=:id", async (req, res) => {
+            const id = req.params.id;
+            console.log(id, "delete");
+            const result = await collectionPost.deleteOne({
+                _id: new ObjectId(id),
+            });
+
+            if (result.deletedCount === 1) {
+                console.log("Successfully deleted one document.");
+                res.header("status", "200");
+                res.send({status: 200, message: "Successfully deleted post."});
+            } else {
+                console.log(
+                    "No documents matched the query. Deleted 0 documents."
+                );
+                res.header("status", "400");
+                res.send({
+                    status: 400,
+                    message: "No Post matched the query. Deleted 0 Post.",
+                });
             }
         });
     } finally {
